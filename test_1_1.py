fcf5c00db4486d1501a009370a01fd23c5b9dac5
cnzjhdx <dingxianghu@outlook.com>
2016-10-23 12:02:06 +0800

    first

---
+++ test_1_1.py
@@ -0,0 +1,12 @@
+a = input("Enter your score : ")
+if a >90 and a<=100:
+    print "Your grade is A"
+elif a >=70 and a<=89:
+    print "Your grade is B"
+elif a >=60 and a<=69:
+    print "Your grade is C"
+elif a >0 and a<=59:
+    print "Your grade is D"
+else:
+    print "Your enter is invalid score"
+
